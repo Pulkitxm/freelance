830c8a4e7e53a8ab3d5b5de95dfcd508714c158b
Pulkit <kpulkit15234@gmail.com>
2024-02-04 00:33:45 +0530

    ui updates

--- eos -architecture/script.js
+++ eos -architecture/script.js
@@ -1,6 +1,5 @@
 const appElement = document.querySelector('.app');
 const navElement = document.querySelector('nav');
-const titleElement = document.getElementById('title');
 const homeElement = document.querySelector('#home');
 const arrow = document.querySelector('#arrow');
 const arrowUp = document.querySelector('#arrowUp');
@@ -11,24 +10,24 @@ const images = document.querySelectorAll('img');
 const videos = document.querySelectorAll('video');
 const slidebarWidth = appElement && (appElement.offsetWidth - appElement.clientWidth);
 const testimonials = document.querySelector('.testimonials');
+const carouselContainer = document.getElementById('carousel-container');
+let currentIndex = 0;
 
 const handleScroll = () => {
     const scrollPosition = appElement && appElement.scrollTop;
 
     // scroll to top arrow handle
-    arrowUp.classList.toggle("show", scrollPosition >= homeHeight);
-    arrowUp.classList.toggle("hide", scrollPosition < homeHeight);
+    arrowUp.classList.toggle("show", scrollPosition >= homeHeight-50);
+    arrowUp.classList.toggle("hide", scrollPosition < homeHeight-50);
 
     // home scroll down arrow styling
     arrow.style.animation = (scrollPosition === 0) ? 'showUpRev 1s forwards' : 'hideDownRev 2s forwards';
 
     //changing nav styles
     const isScrolledTop = scrollPosition < 5;
-    navElement.style.background = isScrolledTop ? 'transparent' : 'rgb(215, 158, 126)';
+    navElement.style.background = isScrolledTop ? 'transparent' : '#D69F7E';
     navElement.style.borderBottom = isScrolledTop ? 'solid 2px #ffffff98' : null;
-    navElement.style.color = isScrolledTop ? 'white' : 'black';
-    titleElement.style.color = isScrolledTop ? 'white' : 'black';
-    titleElement.style.opacity = isScrolledTop ? '.7' : '1';
+    navElement.style.color = isScrolledTop ? 'white' : '#28282B';
 };
 const navigate = (e) => {
     const element = document.getElementById(e.target.dataset.link);
@@ -72,52 +71,55 @@ const handleSmallNavAndResize = () => {
         collapsible.style.display = 'flex';
     }
 };
-const makeTeamElements = () => {
-    const teamItem = document.querySelector('.team-item');
-    const teamContainer = teamItem.parentElement;
-    const team = [
-        {
-            name: 'abc',
-            img: '1.webp',
-        },
-        {
-            name: 'def',
-            img: '2.webp',
-        },
-        {
-            name: 'abc',
-            img: '1.webp',
-        },
-        {
-            name: 'def',
-            img: '2.webp',
-        },
-        {
-            name: 'abc',
-            img: '1.webp',
-        },
-        {
-            name: 'def',
-            img: '2.webp',
-        },
-    ]
-    team.forEach((member) => {
-        const clone = teamItem.cloneNode(true);
-        clone.style.height = '100px';
-        clone.style.width = '100px';
-        clone.querySelector('.team-face').src = `./assets/original/team/${member.img}`;
-        clone.querySelector('.name').innerText = member.name;
-        teamContainer.appendChild(clone);
-    })
-    teamItem.remove();
-}
 const makeReviewsElements = () => {
     const reviewItem = document.querySelector('.review');
     const reviewContainer = reviewItem.parentElement;
-    for (let i = 0; i < 3; i++) {
+    const reviews = [
+        {
+            review: "Eos Architecture has transformed my perspective on natural building. Their workshops are insightful, and the community they've built is inspiring. I'm proud to be a part of this eco-friendly journey.",
+            user: "Alice Johnson"
+        },
+        {
+            review: "Being a part of Eos Architecture's projects has been an enriching experience. Their commitment to sustainable design and harmonious coexistence with the environment is truly commendable.",
+            user: "Charlie Thompson"
+        },
+        {
+            review: "The workshops at Eos Architecture are both educational and enjoyable. The hands-on approach to natural building techniques has empowered me to contribute positively to our environment.",
+            user: "Olivia Davis"
+        },
+        {
+            review: "Studio Eos' projects showcase a unique blend of creativity and sustainability. The team's dedication to creating timeless architectural marvels is evident in every project they undertake.",
+            user: "Daniel Miller"
+        },
+        {
+            review: "I discovered Eos Architecture through their engaging community events. The sense of belonging and shared passion for natural building make it more than just an architecture studio—it's a family.",
+            user: "Sophia Wright"
+        },
+        {
+            review: "Eos Architecture's commitment to using natural pigments and sustainable materials in their projects reflects a deep respect for the Earth. Their work goes beyond aesthetics, embodying principles of environmental consciousness.",
+            user: "James Harris"
+        },
+        {
+            review: "Attending Eos Architecture's workshops was a game-changer for me. The team's expertise and the variety of content in each workshop made the learning experience both enjoyable and valuable.",
+            user: "Emma Martinez"
+        },
+        {
+            review: "The community at Eos Architecture is vibrant and supportive. The shared enthusiasm for eco-friendly design creates an environment where ideas flourish, and collaborations thrive.",
+            user: "Ryan Brooks"
+        },
+        {
+            review: "Studio Eos' dedication to natural building techniques is evident in the Ashreya project at Yaksha Forest Garden. The result is not just a building; it's a testament to sustainable living.",
+            user: "Lily Turner"
+        },
+        {
+            review: "Meeting the team at Eos Architecture was a pleasure. Their passion for natural building and commitment to a zero-cement approach make them leaders in the field. Proud to have collaborated with them.",
+            user: "Maxwell Clark"
+        }
+    ];
+    for (let i = 0; i < reviews.length; i++) {
         const clone = reviewItem.cloneNode(true);
-        clone.querySelector('.card-text').innerText = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus tempora cum beatae pariatur`;
-        clone.querySelector('.card-footer').innerText = `- XYZ`;
+        clone.querySelector('.card-text').innerText = "'' "+reviews[i].review+" ''";
+        clone.querySelector('.card-footer').innerText = '- '+reviews[i].user;
         reviewContainer.appendChild(clone);
     }
     reviewItem.remove();
@@ -127,9 +129,9 @@ const loadBlurryImages = () => {
     blurDivs.forEach(div => {
         const img = div.querySelector("img");
         function loaded() {
-            setTimeout(()=>{
+            setTimeout(() => {
                 div.classList.add("loaded")
-            },500)
+            }, 500)
         }
 
         if (img.complete) {
@@ -140,16 +142,29 @@ const loadBlurryImages = () => {
         }
     })
 }
+function moveCarousel(direction) {
+    currentIndex = (currentIndex + direction + carouselContainer.children.length) % carouselContainer.children.length;
+    const translateValue = -currentIndex * 100 + '%';
+    // if(carouselContainer.children[currentIndex].classList.contains('loaded')){
+        carouselContainer.style.transform = 'translateX(' + translateValue + ')';
+    // }else{
+    //     carouselContainer.children[currentIndex].children[0].addEventListener("load", () => {
+    //        setTimeout(() => { carouselContainer.style.transform = 'translateX(' + translateValue + ')';},500);
+    //     });
+    // }
+}
 
-try {
+// try {
     window.addEventListener("load", () => {
-        document.querySelector('#loading').style.opacity = '0';
-        document.querySelector('#loading').style.zIndex = '0';
-        document.querySelector('#loading').style.display = 'none';
+        setTimeout(() => {
+            // document.querySelector('#loading').style.opacity = '0';
+            // document.querySelector('#loading').style.zIndex = '0';
+            // document.querySelector('#loading').style.display = 'none';
+        }, 2000);
     })
     handleScroll();
     loadBlurryImages();
-    arrow.addEventListener('click', () => appElement.scrollTo({ top: window.innerHeight, behavior: "smooth" }));
+    arrow.addEventListener('click', () => appElement.scrollTo({ top: window.innerHeight-50, behavior: "smooth" }));
     arrowUp.addEventListener('click', () => appElement.scrollTo({ top: 0, behavior: "smooth" }));
     appElement.addEventListener("scroll", handleScroll);
     navigators.forEach(navigator => {
@@ -161,14 +176,12 @@ try {
     if (window.innerWidth > 1270) {
         navElement.style.width = `calc(100vw - ${slidebarWidth}px)`;
     }
-    if (window.innerWidth < 700) {
-        testimonials.parentNode.removeChild(testimonials);
-    }
-    makeTeamElements();
     makeReviewsElements();
-} catch (err) { }
+    const interval = 2000;
+    setInterval(() => moveCarousel(1), interval);
+// } catch (err) { }
 
-// appElement && appElement.scrollTo({
-//     top: document.getElementById('contact').getBoundingClientRect().top,
-//     behavior: "smooth"
-// });
+appElement && appElement.scrollTo({
+    top: document.querySelector('.commuity').getBoundingClientRect().top-100,
+    behavior: "smooth"
+});
