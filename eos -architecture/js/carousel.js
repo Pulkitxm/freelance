e4c04f3b7f3e119fc97fea05a00b7f49530d997d
Pulkit <kpulkit15234@gmail.com>
2024-06-01 12:29:22 +0530

    testimonials design implementation

--- eos -architecture/js/carousel.js
+++ eos -architecture/js/carousel.js
@@ -5,7 +5,9 @@ class Position {
         this.position = 0;
         this.carousel = document.querySelector('.flickity-slider');
         this.max = this.carousel.children.length;
-        chnageHighlight(null, this.carousel.children[0]);
+        const left = this.position - 1 < 0 ? this.max - 1 : this.position - 1;
+        const right = this.position + 1 > this.max - 1 ? 0 : this.position + 1;
+        changeHighlight(null, this.carousel.children[this.position], this.carousel.children[left], this.carousel.children[right]);
     }
     increment() {
         const a = this.position;
@@ -14,7 +16,9 @@ class Position {
         } else {
             this.position = 0;
         }
-        chnageHighlight(this.carousel.children[a], this.carousel.children[this.position]);
+        const left = this.position - 1 < 0 ? this.max - 1 : this.position - 1;
+        const right = this.position + 1 > this.max - 1 ? 0 : this.position + 1;
+        changeHighlight(this.carousel.children[a], this.carousel.children[this.position], this.carousel.children[left], this.carousel.children[right]);
     }
     decrement() {
         const a = this.position;
@@ -23,21 +27,31 @@ class Position {
         } else {
             this.position = this.max - 1;
         }
-        chnageHighlight(this.carousel.children[a], this.carousel.children[this.position]);
-    }
-    setPosition(value) {
-        this.position = value;
-        chnageHighlight(null, this.carousel.children[this.position]);
+        const left = this.position - 1 < 0 ? this.max - 1 : this.position - 1;
+        const right = this.position + 1 > this.max - 1 ? 0 : this.position + 1;
+        changeHighlight(this.carousel.children[a], this.carousel.children[this.position], this.carousel.children[left], this.carousel.children[right]);
     }
 }
 
-function chnageHighlight(a, b) {
-    const isMobile= /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    if(!isMobile){
+function changeHighlight(a, b, c, d) {
+    console.log("left", c);
+    console.log("right", d);
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    if (!isMobile) {
         if (a) {
             a.classList.remove("highlight-carousel");
         }
+        
+        b.classList.remove("gallery-cell-left");
+        b.classList.remove("gallery-cell-right");
+        c && c.classList.remove("gallery-cell-left");
+        c && c.classList.remove("gallery-cell-right");
+        d && d.classList.remove("gallery-cell-right");
+        d && d.classList.remove("gallery-cell-left");
+        
         b.classList.add("highlight-carousel");
+        c && c.classList.add("gallery-cell-left");
+        d && d.classList.add("gallery-cell-right");
     }
 }
 
