3d7f542973037adfe7264dbea3697443911f6146
Pulkit <kpulkit15234@gmail.com>
2024-06-18 13:29:17 +0530

    hero section with parllax

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("scroll", function () {
+  const parallaxElements = document.querySelectorAll(".parallax-bg");
+  parallaxElements.forEach(function (element) {
+    let scrollPosition = window.pageYOffset;
+    element.style.transform = "translateY(" + scrollPosition * 0.5 + "px)";
+  });
+});
