b00e4191cafd971177af52e8152ba5a0ecdb51a0
Pulkit <kpulkit15234@gmail.com>
2024-06-18 13:39:08 +0530

    smooth scroll with lenis scroll

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+const navLogo = document.querySelector("#logo");
+const parallaxElements = document.querySelectorAll(".parallax-bg");
+const lenis = new Lenis()
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
+
 window.addEventListener("scroll", function () {
-  const parallaxElements = document.querySelectorAll(".parallax-bg");
   parallaxElements.forEach(function (element) {
     let scrollPosition = window.pageYOffset;
     element.style.transform = "translateY(" + scrollPosition * 0.5 + "px)";
+  });
+});
+
+navLogo.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
   });
 });
