412c371b4c2c344a60bd531902e590632a8422de
Pulkit <kpulkit15234@gmail.com>
2024-06-19 11:51:48 +0530

    meet team modal

---
+++ data/desc.js
@@ -0,0 +1,20 @@
+const desc = [
+  {
+    name: "Saumya Grover",
+    desc: "Meet Saumya Grover, the visionary behind EOS. As an architect driven by a passion for sustainability, she specializes in zero cement projects, dedicated to minimizing carbon footprints. With expertise in designing stunning natural buildings using mud, stone, and lime, she brings eco-consciousness to architectural excellence.",
+  },
+  {
+    name: "Shad Fatima",
+    desc: "Introducing Shad Fatima, a multifaceted artist renowned for her prowess in painting, ceramics, and more. With accolades including selection for the prestigious 7th Beijing International Art Biennale China and awards like the SH Raza Award in Painting and Avishkar Van Gogh Award, she adds artistic flair to natural building projects, crafting exquisite wall arts and décor from mud.",
+  },
+  {
+    name: "Yuvraj Naruka",
+    desc: "Meet Yuvraj Naruka, India's budding Permaculturist extraordinaire. His holistic approach to unbuilt design, rooted in permaculture principles, sets him apart. Whether it's creating vibrant vegetable gardens or enchanting forest landscapes, Yuvraj tailors his designs to suit the unique needs of each project, ensuring harmony between nature and architecture.",
+  },
+  {
+    name: "Vaageesha Singh",
+    desc: "This is Vaageesha Singh, an Interior Designer with a passion for sustainable growth. She specializes in space planning and furniture design, crafting spaces that seamlessly blend homeliness with aesthetic functionality. She ensures your project reflects your unique style and values, providing you with beautiful and customised solutions, all of that with",
+  },
+];
+
+export default desc;
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 // import "./scroll.js";
 import "./parallax.js";
 import "./lighGallery.js";
+import desc from "../data/desc.js";
 
 // section headers style
 const sectionHeaders = document.querySelectorAll(".section-header");
@@ -25,4 +26,23 @@ navbarButton.addEventListener("click", () => onToggleMenu(navbarButton));
 const navLinks = document.querySelectorAll(".nav-link");
 navLinks.forEach((link) => {
   link.addEventListener("click", () => onToggleMenu(navbarButton));
+});
+
+// show team member
+const teamMembers = document.querySelectorAll(".team-member");
+const teamMemberModal = document.getElementById("show-team-member");
+teamMembers.forEach((member) => {
+  member.addEventListener("click", () => {
+    const name = member.getAttribute("data-name");
+    teamMemberModal.classList.remove("hidden");
+    teamMemberModal.querySelector("img").src = member.querySelector("img").src;
+    teamMemberModal.querySelector("p").textContent = desc.find(
+      (d) => d.name === name
+    ).desc;
+  });
+});
+
+const closeModalButton = document.getElementById("close-team-member-popup");
+closeModalButton.addEventListener("click", () => {
+  teamMemberModal.classList.add("hidden");
 });
