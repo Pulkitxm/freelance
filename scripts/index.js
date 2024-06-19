0c951f15baa1434f89a0467e9537c56807b35c4a
Pulkit <kpulkit15234@gmail.com>
2024-06-19 10:27:32 +0530

    toggle nav

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,22 @@ const sectionHeaders = document.querySelectorAll(".section-header");
 sectionHeaders.forEach((header) => {
   header.style.fontFamily = "Pacifico, sans-serif";
 });
+
+function onToggleMenu(button) {
+  const navbarDropdown = document.getElementById("navbar-dropdown");
+  const isExpanded = button.getAttribute("aria-expanded") === "true";
+
+  button.setAttribute("aria-expanded", !isExpanded);
+
+  if (isExpanded) {
+    navbarDropdown.classList.add("hidden");
+  } else {
+    navbarDropdown.classList.remove("hidden");
+  }
+}
+const navbarButton = document.getElementById("navbar-toggle");
+navbarButton.addEventListener("click", () => onToggleMenu(navbarButton));
+const navLinks = document.querySelectorAll(".nav-link");
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => onToggleMenu(navbarButton));
+});
